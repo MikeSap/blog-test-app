fc03b4538831a80ecd5a799a0a9538e19c0b5af9
mike <mikesapienzaaudio@gmail.com>
2021-02-19 13:34:03 -0600

    navigator setup

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  );
+};
+
+export default IndexScreen;
+
+const styles = StyleSheet.create({});
